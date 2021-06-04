dc1f04ddb0e7b7e53b66e7ccf72a1bbabbd82a6a
o9uzdev <o9uzdev@gmail.com>
2021-06-04 20:40:29 +0300

    Create App class

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Code
+    }
+}
