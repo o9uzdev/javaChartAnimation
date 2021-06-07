3680d9a48977b92c5619f1b257ffa60159d13470
o9uzdev <o9uzdev@gmail.com>
2021-06-07 23:05:35 +0300

    Added rectangle and circle on scene

--- src/com/javaChartAnimation/App.java
+++ src/com/javaChartAnimation/App.java
@@ -3,16 +3,26 @@ package com.javaChartAnimation;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Rectangle rectangle = new Rectangle(100,100,100,100);
+        rectangle.setFill(Color.BLUE);
+        Circle circle = new Circle(100,100,100);
+        
         stage.setTitle("Java Chart Animation");
 
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane, 800, 600);
+        Pane root = new Pane();
+        root.getChildren().addAll(circle, rectangle);
+
+        stage.setScene(new Scene(root, 600, 600));
         stage.show();
     }
 }
